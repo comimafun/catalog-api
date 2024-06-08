d749227beec664f42e217bbdc51b51f1f7674cca
Althaf Demiandra <althafdemiandraakbar@gmail.com>
2024-06-08 15:42:39 +0700

    fix: migrations files (#9)

--- migrator/migrations/20240608024409_circle_fandom.up.sql
+++ migrator/migrations/20240608024409_circle_fandom.up.sql
@@ -3,6 +3,7 @@ create table
         "circle_id" int not null,
         "fandom_id" int not null,
         "created_at" timestamp not null default current_timestamp,
-        "updated_at" timestamp not null default current_timestamp,
-        primary key ("circle_id", "fandom_id")
+        primary key ("circle_id", "fandom_id"),
+        foreign key ("circle_id") references "circle" ("id") on delete cascade,
+        foreign key ("fandom_id") references "fandom" ("id") on delete cascade
     );
--- migrator/migrations/20240608054410_circle_work_type.up.sql
+++ migrator/migrations/20240608054410_circle_work_type.up.sql
@@ -3,6 +3,7 @@ create table
         circle_id integer not null,
         work_type_id integer not null,
         created_at timestamp not null default current_timestamp,
-        updated_at timestamp not null default current_timestamp,
-        primary key (circle_id, work_type_id)
+        primary key (circle_id, work_type_id),
+        foreign key (circle_id) references "circle" (id) on delete cascade,
+        foreign key (work_type_id) references "work_type" (id) on delete cascade
     )
