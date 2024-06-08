0af1c625dfdbeb557352e1f5a580ab9d363ea633
althafdaa <althafdemiandraakbar@gmail.com>
2024-06-08 13:34:21 +0700

    fix: migration scripts

--- migrator/migrations/20240608054410_circle_work_type.down.sql
+++ migrator/migrations/20240608054410_circle_work_type.down.sql
@@ -1,8 +1,1 @@
-create table
-    "circle_work_type" (
-        circle_id integer not null,
-        work_type_id integer not null,
-        created_at timestamp not null default current_timestamp,
-        updated_at timestamp not null default current_timestamp,
-        primary key (circle_id, work_type_id),
-    )
+drop table if exists "circle_work_type";
--- migrator/migrations/20240608054410_circle_work_type.up.sql
+++ migrator/migrations/20240608054410_circle_work_type.up.sql
@@ -1,1 +1,8 @@
-drop table if exists "circle_work_type";
+create table
+    "circle_work_type" (
+        circle_id integer not null,
+        work_type_id integer not null,
+        created_at timestamp not null default current_timestamp,
+        updated_at timestamp not null default current_timestamp,
+        primary key (circle_id, work_type_id)
+    )
