e8e584cf90f2dfbc162893b6bb6308205e41524d
althafdaa <althafdemiandraakbar@gmail.com>
2024-07-29 19:52:17 +0700

    test: put test inside users_test for user package

--- tests/user_test.go
+++ tests/user_test.go
@@ -1,25 +1,111 @@
-package tests
+package user_test
 
 import (
 	"catalog-be/internal/database"
 	"catalog-be/internal/entity"
 	"catalog-be/internal/modules/user"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"gorm.io/gorm"
 )
+
+var connUrl = ""
+var sleepTime = time.Millisecond * 5
+
+func migrate(db *gorm.DB) {
+	sqlDir := "../migrator/migrations"
+
+	files, err := os.ReadDir(sqlDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), "up.sql") {
+			sqlContent, err := os.ReadFile(fmt.Sprintf("%s/%s", sqlDir, file.Name()))
+			if err != nil {
+				panic(err)
+			}
+
+			err = db.Exec(string(sqlContent)).Error
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("Migrated: %s\n", file.Name())
+		}
+	}
+}
+
+func setupDb(dsn string) *gorm.DB {
+	db := database.New(dsn)
+	migrate(db)
+	return db
+}
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	container, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:16"),
+		postgres.WithDatabase("testdb"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("foobar"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second),
+		),
+	)
+
+	if err != nil {
+		log.Fatalf("Could not start postgres container: %s", err)
+	}
+
+	connUrl, err = container.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		log.Fatalf("Could not get connection string: %s", err)
+	}
+	setupDb(connUrl)
+	res := m.Run()
+	os.Exit(res)
+}
 
 func TestUser(t *testing.T) {
 	db := database.New(connUrl)
 	userRepo := user.NewUserRepo(db)
 
-	as, err := userRepo.CreateOne(entity.User{
-		Name: "john doe",
+	service := user.NewUserService(userRepo)
+
+	t.Run("Create new user", func(t *testing.T) {
+		as, err := service.CreateOne(entity.User{
+			Name:  "john doe",
+			Email: "test@test.com",
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, as)
+
+		time.Sleep(sleepTime)
 	})
 
-	assert.Nil(t, err)
-	assert.NotNil(t, as)
+	t.Run("Get user by email", func(t *testing.T) {
+		as, err := service.FindOneByEmail("test@test.com")
 
-	time.Sleep(sleepTime)
+		assert.Nil(t, err)
+		assert.NotNil(t, as)
+
+		assert.Equal(t, "test@test.com", as.Email)
+		assert.Equal(t, 1, as.ID)
+	})
 }
